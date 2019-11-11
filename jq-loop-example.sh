f368632772d8ae220ae97a97aa13fb754bf9363f
Raimon Bosch <raimon.bosch@gmail.com>
2019-11-11 17:12:14 +0100

    Update jq-loop-example.sh

--- jq-loop-example.sh
+++ jq-loop-example.sh
@@ -2,9 +2,9 @@
 
 ids=$(curl -XGET "http://elastic-host:9200/my_index/_search/?stored_fields=id&sort=id&size=10&from=0" | jq -r '.hits.hits[]._id' | xargs -n1 echo | cat)
 
-for i in $ids
+for id in $ids
 do
-    echo "Reindexing id $i.."
+    echo "Operate over $id.."
     #Here you can do your operation
 done
 
