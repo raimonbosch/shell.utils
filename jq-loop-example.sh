a3bedb5fff8bcc280c9136050e994dc490287c6f
Raimon Bosch <raimon.bosch@gmail.com>
2019-11-11 17:25:26 +0100

    Update jq-loop-example.sh

--- jq-loop-example.sh
+++ jq-loop-example.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Script that shows how you can parse a JSON from shell and create a for loop
 ids=$(curl -XGET "http://elastic-host:9200/my_index/_search/?stored_fields=id&sort=id&size=10&from=0" | jq -r '.hits.hits[]._id' | xargs -n1 echo | cat)
 
 for id in $ids
